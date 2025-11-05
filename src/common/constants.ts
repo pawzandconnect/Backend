fe3d4bbcc4d229e210c6922876d838bf14353f0a
biggaji <tobiajibade2017@yandex.com>
2025-11-05 18:01:51 +0100

    feat: adjust file size constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -6,6 +6,8 @@ export enum PlatformEvents {
 // Photos : 5MB each, max 5 photos
 // Video: 20MB max
 const MAX_PHOTOS = 5;
+const MAX_PHOTO_SIZE = 5;
 const MAX_SIZE_MB = 5 * 1024 * 1024; // 5MB in bytes
 const MAX_VIDEO_SIZE_MB = 20 * 1024 * 1024; // 20MB in bytes
-export { MAX_PHOTOS, MAX_SIZE_MB, MAX_VIDEO_SIZE_MB };
+const MAX_VIDEO_SIZE = 20;
+export { MAX_PHOTOS, MAX_SIZE_MB, MAX_VIDEO_SIZE_MB, MAX_PHOTO_SIZE, MAX_VIDEO_SIZE };
